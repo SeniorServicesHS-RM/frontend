889ffb02b37271f570bd420c6bdd6b20a758bc5f
Marcel Mehl <marcel.mehl@ibs-banking.com>
2022-12-09 14:15:01 +0100

    created a Class for Articles

--- src/data/Article.ts
+++ src/data/Article.ts
@@ -1,5 +1,41 @@
 class Article {
-  constructor() {}
+  constructor(
+    private _id: string,
+    private _name: string,
+    private _note?: string,
+    private _category?: string,
+    private _picture?: string
+  ) {}
+  public get id() {
+    return this._id;
+  }
+  public set id(id: string) {
+    this._id = id;
+  }
+  public get name() {
+    return this._name;
+  }
+  public set name(name: string) {
+    this._name = name;
+  }
+  public get note() {
+    return this._note;
+  }
+  public set note(note: string) {
+    this._note = note;
+  }
+  public get category() {
+    return this._category;
+  }
+  public set category(category: string) {
+    this._category = category;
+  }
+  public get picture() {
+    return this._picture;
+  }
+  public set picture(picture: string) {
+    this._picture = picture;
+  }
 }
 
 export default Article;
