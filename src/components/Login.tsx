725a0db2d8d5656539c7d2ee7cec8b6bb8974e0f
AppletGGGGG <f.schuemann@hotmail.de>
2022-12-17 01:02:50 +0100

    added new Login Page and Component

---
+++ src/components/Login.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { NavLink } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+
+//import './LoginPage.css';
+
+const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Hier könnte man dann zum Beispiel eine API-Anfrage an denpn Server senden,
+    // um sich anzumelden. Wenn die Anmeldung erfolgreich war, könnte man dann
+    // mit "setIsLoggedIn(true)" den "isLoggedIn"-Status auf "true" setzen und
+    // mit <NavLink to="/dashboard"> zu einer Dashboard-Seite weiterleiten.
+    navigate('/');
+  };
+
+  return (
+    <form className="login-form" noValidate autoComplete="off" onSubmit={handleSubmit}>
+      <div>
+        <TextField
+          label="E-Mail"
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <TextField
+          label="Passwort"
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <Button variant="contained" type="submit">
+          Anmelden
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default LoginPage;
--- src/data/DrawerData.tsx
+++ src/data/DrawerData.tsx
@@ -1,6 +1,7 @@
 import HouseIcon from "@mui/icons-material/House";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import AccessibleIcon from "@mui/icons-material/Accessible";
+import LoginIcon from '@mui/icons-material/Login';
 
 const DrawerData = [
   {
@@ -17,6 +18,11 @@ const DrawerData = [
     title: "Services",
     path: "/services",
     icon: <AccessibleIcon />,
+  },
+  {
+    title: "Login",
+    path: "/login",
+    icon: <LoginIcon />,
   },
 ];
 
