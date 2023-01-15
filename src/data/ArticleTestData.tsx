6c6107a01570187e764b0ecd3ce9f120bf7e6d25
Marcel Mehl <marcel.mehl@ibs-banking.com>
2023-01-15 19:01:36 +0100

    resolved minor merge bugs

--- src/data/ArticleTestData.tsx
+++ src/data/ArticleTestData.tsx
@@ -1,52 +1,45 @@
 import Article from "./Article";
 import Order from "./Order";
-// export const OrderArray: Order[] = [
-//   new Order(
-//     "o1",
-//     "senior1",
-//     new Article("1", "Banane",6,"Aldi", "Gelbe"),
-//     new Date(),
-//   ),
-//   new Order(
-//     "o2",
-//     "senior2",
-//     new Article("2", "Apfel", "Grüne"),
-//     6,
-//     new Date(),
-//     "Rewe"
-//   ),
-//   new Order(
-//     "o3",
-//     "senior3",
-//     new Article("3", "Keks", "Schokolade"),
-//     6,
-//     new Date(),
-//     "Kaufland"
-//   ),
-//   new Order(
-//     "o4",
-//     "senior4",
-//     new Article("4", "Milch", "Weiß"),
-//     6,
-//     new Date(),
-
-    "Lidl"
-  ),
+export const OrderArray: Order[] = [
+  //   new Order(
+  //     "o1",
+  //     "senior1",
+  //     new Article("1", "Banane",6,"Aldi", "Gelbe"),
+  //     new Date(),
+  //   ),
+  //   new Order(
+  //     "o2",
+  //     "senior2",
+  //     new Article("2", "Apfel", "Grüne"),
+  //     6,
+  //     new Date(),
+  //     "Rewe"
+  //   ),
+  //   new Order(
+  //     "o3",
+  //     "senior3",
+  //     new Article("3", "Keks", "Schokolade"),
+  //     6,
+  //     new Date(),
+  //     "Kaufland"
+  //   ),
+  //   new Order(
+  //     "o4",
+  //     "senior4",
+  //     new Article("4", "Milch", "Weiß"),
+  //     6,
+  //     new Date(),
   new Order(
     "o5",
     "senior5",
-    new Article("5", "Mandarine", "Orange"),
-    6,
-    new Date(),
-    "Lidl"
+    [new Article(Date.now().toString(), "Mandarine", 5, "Lidl")],
+    new Date()
   ),
   new Order(
     "o6",
     "senior5",
-    new Article("5", "Mandarine2", "Orange"),
-    6,
-    new Date(),
-    "Edeka"
+    [new Article(Date.now().toString(), "Mandarine2", 5, "Aldi")],
+    new Date()
   ),
 ];
 export const MartAry: String[] = ["Rewe", "Aldi", "Lidl", "Edeka", "Kaufland"];
