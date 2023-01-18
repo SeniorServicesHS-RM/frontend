2a699fba1aa8dfb23fb54fb4260e2fb33592ba01
AppletGGGGG <f.schuemann@hotmail.de>
2023-01-18 15:39:06 +0100

    Aufräumen

--- src/pages/NavigationPage.tsx
+++ src/pages/NavigationPage.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import ServicesPage from "./ServicesPage";
-import SignIn from "./SignIn";
-
 import PlanningPage from "./PlanningPage";
 import LoginPage from "./LoginPage";
 import ShoppingPageSelection from "./ShoppingPageSelection";
+
 const NavigationPage = () => {
   return (
     <React.Fragment>
@@ -15,7 +14,6 @@ const NavigationPage = () => {
         <Route path="/shopping" element={<ShoppingPageSelection />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/signin" element={<SignIn />} />
         <Route path="/planning" element={<PlanningPage />} />
       </Routes>
     </React.Fragment>
