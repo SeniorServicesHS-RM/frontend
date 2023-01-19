3c336cf20947b2920cc586be4ad1ebe9eb2d6d50
AppletGGGGG <f.schuemann@hotmail.de>
2023-01-19 02:48:57 +0100

    authism centering

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,9 @@
 import { Button, Grid } from "@mui/material";
 import FlexBox from "../components/FlexBox";
 import { useNavigate } from "react-router-dom";
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
 
 const LandingPage = () => {
   let navigate = useNavigate();
@@ -10,12 +13,23 @@ const LandingPage = () => {
   };
 
   return (
-    <FlexBox>
-      <p>Hier können Sie sich einloggen UwU:</p>
-      <Button variant="contained" onClick={routeChange}>
-        Login
-      </Button>
-    </FlexBox>
+    <Paper component={Stack} direction="column" justifyContent="center">
+      <FlexBox>
+        <Typography variant="h5" component="h3">
+          H.G. Buddne
+        </Typography> 
+      </FlexBox>
+      <FlexBox>
+        <p>Hier können Sie sich einloggen UwU:</p>
+      </FlexBox>
+      <FlexBox>
+        <Button variant="contained" onClick={routeChange}>
+            Login
+        </Button>
+      </FlexBox>
+    </Paper>
+
+
   );
 };
 
