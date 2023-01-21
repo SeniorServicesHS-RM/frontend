7a40e9e289b5028044948ed410642ebf958a588a
Dominik <dominik.oehrlein@student.hs-rm.de>
2023-01-21 20:31:09 +0100

    added additional Services to Emp

--- src/components/ShowOrdersBySeniors.tsx
+++ src/components/ShowOrdersBySeniors.tsx
@@ -1,10 +1,20 @@
-import { Card, Grid, TextField, Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  Dialog,
+  DialogActions,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Button from "@mui/material/Button";
 import CardContent from "@mui/material/CardContent/CardContent";
 import { useState } from "react";
 import Article from "../data/Article";
 import Order from "../data/Order";
 import FlexBox from "./FlexBox";
 import ShowArticles from "./ShowArticles";
+import ShowUserOverview from "./ShowUserOverview";
 
 interface Props {
   senior: string;
@@ -14,47 +24,67 @@ interface Props {
 }
 
 const ShowOrdersBySeniors = (props: Props) => {
-  const getUserOrdersByEmp = () => {
-    return props.orderList
-      ? props.orderList.filter((order) => {
-          return order.seniorId === props.senior;
-        })
-      : [];
+  const [showSeniorOverview, setShowSeniorOverview] = useState(false);
+
+  const openSeniorOverview = () => {
+    setShowSeniorOverview(true);
+  };
+  const closeSeniorOverview = () => {
+    setShowSeniorOverview(false);
   };
 
-  const userOrders = getUserOrdersByEmp();
-  const getAllArticles = () => {
+  const getEmpArticlesBySeniorAndMart = () => {
     const newList = [] as Article[];
-    userOrders.map((order) => {
+    props.orderList.map((order) => {
       order.articleList.map((article) => {
-        // if (order.seniorId === props.senior) {
-        if (article.mart === props.selectedMart) {
+        if (
+          order.seniorId === props.senior &&
+          article.mart === props.selectedMart
+        ) {
           newList.push(article);
         }
-        // }
       });
     });
+
     return newList;
+  };
+  const getEmpOrdersBySenior = () => {
+    return props.orderList.filter((order) => {
+      return order.seniorId === props.senior;
+    });
   };
 
   return (
     <>
       {props.showSorted ? (
-        <Card variant="outlined">
-          <CardContent>
-            <Typography
-              variant="h3"
-              sx={{ p: 1, fontSize: 22, fontWeight: "bold" }}
-              color="text.secondary"
-              gutterBottom
-            >
-              {props.senior}
-            </Typography>
-            <ShowArticles articles={getAllArticles()}></ShowArticles>
-          </CardContent>
-        </Card>
+        <>
+          <Card variant="outlined">
+            <CardContent>
+              <CardActionArea onClick={openSeniorOverview}>
+                <Typography
+                  variant="h3"
+                  sx={{ p: 1, fontSize: 22, fontWeight: "bold" }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  {props.senior}
+                </Typography>
+              </CardActionArea>
+
+              <ShowUserOverview
+                abort={closeSeniorOverview}
+                open={showSeniorOverview}
+                seniorId={props.senior}
+                orderList={getEmpOrdersBySenior()}
+              />
+              <ShowArticles
+                articles={getEmpArticlesBySeniorAndMart()}
+              ></ShowArticles>
+            </CardContent>
+          </Card>
+        </>
       ) : (
-        <ShowArticles articles={getAllArticles()}></ShowArticles>
+        <ShowArticles articles={getEmpArticlesBySeniorAndMart()} />
       )}
     </>
   );
---
+++ src/components/ShowUserOverview.tsx
@@ -0,0 +1,55 @@
+import {
+  Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControlLabel,
+  TextField,
+} from "@mui/material";
+import Button from "@mui/material/Button";
+import { useContext, useState } from "react";
+import Order from "../data/Order";
+import { DataBaseContext } from "../store/DataBaseContext";
+
+interface Props {
+  abort: () => void;
+  seniorId: string;
+  open: boolean;
+  orderList: Order[];
+}
+const ShowUserOverview = (props: Props) => {
+  const { serviceList } = useContext(DataBaseContext);
+  const getAllAdditionalServices = () => {
+    const newList = [] as string[];
+    props.orderList.map((order: Order) => {
+      order.additionalServices.map((service: string) => {
+        console.log(service);
+        return newList.push(service);
+      });
+    });
+    return newList;
+  };
+
+  const mappedServices = getAllAdditionalServices().map(
+    (singleServ: string) => {
+      return (
+        <FormControlLabel
+          control={<Checkbox checked name={singleServ} />}
+          label={singleServ}
+        />
+      );
+    }
+  );
+  return (
+    <div>
+      <Dialog open={props.open} onClose={props.abort}>
+        <DialogContent>{mappedServices}</DialogContent>
+        <DialogActions>
+          <Button onClick={props.abort}>Zurück</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+export default ShowUserOverview;
