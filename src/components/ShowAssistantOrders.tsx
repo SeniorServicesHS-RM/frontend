6f068840e933ba5958c877a4461981be86dbcae1
Dominik <dominik.oehrlein@student.hs-rm.de>
2023-01-23 19:45:58 +0100

    removed mocking data

--- src/components/ShowAssistantOrders.tsx
+++ src/components/ShowAssistantOrders.tsx
@@ -11,11 +11,13 @@ import {
   ToggleButton,
   ToggleButtonGroup,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Article from "../data/Article";
 import GetEmployeeOrders from "../data/GetEmployeeOrders";
 import Order from "../data/Order";
+import User from "../data/User";
 import { DataBaseContext } from "../store/DataBaseContext";
+import { UserContext } from "../store/UserContext";
 import ArticleCard from "./ArticleCard";
 import FlexBox from "./FlexBox";
 import ShowArticles from "./ShowArticles";
@@ -29,10 +31,15 @@ interface ValueHandler {
   selectedMart: string;
 }
 const ShowAsisstantOrders = (props: Props) => {
-  const employee = "emp001";
-  const seniorList = ["s001", "s002"];
-  const { martList } = React.useContext(DataBaseContext);
-  const [isSorted, setIsSorted] = useState(true);
+  const { user } = useContext(UserContext);
+  const { users } = useContext(DataBaseContext);
+  const employee = user.empID;
+  console.log(user);
+  const seniorList = users.filter((singleUser: User) => {
+    return singleUser.role === 3;
+  });
+
+  const { martList } = useContext(DataBaseContext);
   const [valueMart, setValueMart] = useState<ValueHandler>({
     selectedMart: martList ? martList[0] : "",
   });
@@ -50,10 +57,10 @@ const ShowAsisstantOrders = (props: Props) => {
   const mappedArticleList =
     seniorList &&
     seniorList.length > 0 &&
-    seniorList.map((senior: string) => {
+    seniorList.map((myUser: User) => {
       return (
         <ShowOrdersBySeniors
-          senior={senior}
+          senior={myUser.seniorId}
           orderList={orderList}
           selectedMart={valueMart.selectedMart}
           showSorted={showSorted}
