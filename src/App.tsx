c12f5bd85d12afa182dba97c263aa1095fa73662
Lena-Gottosmile <phuong-lena@hotmail.de>
2023-01-29 19:10:17 +0100

    changing Themes through LogIn

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
 import { BrowserRouter } from "react-router-dom";
-import { blue, indigo, green, deepOrange, grey } from "@mui/material/colors";
 import React from "react";
-import { ThemeProvider } from "@mui/material/styles";
 import MainPage from "./pages/MainPage";
 import { DataBaseProvider } from "./store/DataBaseContext";
-import seniorTheme from "./theme";
-import plannerTheme from "./theme";
-import ekhTheme from "./theme";
+import { ThemeProvider } from "@mui/material/styles";
 import AuthContext from "./store/AuthContext";
 import { UserProvider } from "./store/UserContext";
+import plannerTheme from "./themes/planningTheme";
 
 function App() {
   return (
     <div>
       <UserProvider>
-        <ThemeProvider theme={ekhTheme}>
-          <DataBaseProvider>
-            <BrowserRouter>
-              <MainPage></MainPage>
-            </BrowserRouter>
-          </DataBaseProvider>
-        </ThemeProvider>
+        <DataBaseProvider>
+          <BrowserRouter>
+            <MainPage></MainPage>
+          </BrowserRouter>
+        </DataBaseProvider>
       </UserProvider>
     </div>
   );
 }
 
 export default App;
-//<ThemeProvider theme={plannerTheme}>
-//      <ThemeProvider theme={ekhTheme}>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -12,37 +12,20 @@ import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { ThemeProvider } from "@mui/material/styles";
-import seniorTheme from "../theme";
+import { CustomThemeContext } from "../themes/themeContext";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../store/Firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../store/UserContext";
 import IconButton from "@mui/material/IconButton";
 import InputAdornment from "@mui/material/InputAdornment";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 
-// function Copyright(props: any) {
-//   return (
-//     <Typography
-//       variant="body2"
-//       color="text.secondary"
-//       align="center"
-//       {...props}
-//     >
-//       {"Copyright © "}
-//       <Link color="inherit" href="https://mui.com/">
-//         Your Website
-//       </Link>{" "}
-//       {new Date().getFullYear()}
-//       {"."}
-//     </Typography>
-//   );
-// }
-
 export default function LoginSide() {
   //const [authenticated, setAuthenticated] = useState<boolean>(false);
+  const { user } = useContext(UserContext);
   const { handleUserId } = React.useContext(UserContext);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -52,6 +35,7 @@ export default function LoginSide() {
   const handleMouseDownPassword = () => setShowPassword(!showPassword);
   const [errorPW, setErrorPW] = useState(false);
   const [errorMessagePW, setErrorMessagePW] = useState("");
+  const { currentTheme, setTheme } = useContext(CustomThemeContext);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     setErrorPW(false);
@@ -82,113 +66,124 @@ export default function LoginSide() {
       });
   };
 
+  function switchTheme() {
+    switch (user.role) {
+      case 1:
+        return setTheme("planningTheme");
+      case 2:
+        return setTheme("ekhTheme");
+      case 3:
+        return setTheme("seniorTheme");
+      default:
+        return setTheme("");
+    }
+  }
+
   return (
-    <ThemeProvider theme={seniorTheme}>
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: "url(https://source.unsplash.com/random)",
-            backgroundRepeat: "no-repeat",
-            /*backgroundColor: (t) =>
+    <Grid container component="main" sx={{ height: "100vh" }}>
+      <CssBaseline />
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: "url(https://source.unsplash.com/random)",
+          backgroundRepeat: "no-repeat",
+          /*backgroundColor: (t) =>
               t.palette.mode === "light"
                 ? t.palette.grey[50]
                 : t.palette.grey[900],*/
-            backgroundSize: "cover",
-            backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        >
+          <Avatar sx={{ m: 1 }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
           <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 1 }}
           >
-            <Avatar sx={{ m: 1 }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
+            <TextField
+              error={errorPW}
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={(event) => setEmail(event.target.value)}
+              value={email}
+            />
+            <TextField
+              error={errorPW}
+              helperText={errorMessagePW}
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type={showPassword ? "text" : "password"}
+              id="password"
+              autoComplete="current-password"
+              onChange={(event) => setPassword(event.target.value)}
+              value={password}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {showPassword ? (
+                        <VisibilityIcon />
+                      ) : (
+                        <VisibilityOffIcon />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={() => switchTheme()}
             >
-              <TextField
-                error={errorPW}
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                onChange={(event) => setEmail(event.target.value)}
-                value={email}
-              />
-              <TextField
-                error={errorPW}
-                helperText={errorMessagePW}
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type={showPassword ? "text" : "password"}
-                id="password"
-                autoComplete="current-password"
-                onChange={(event) => setPassword(event.target.value)}
-                value={password}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                      >
-                        {showPassword ? (
-                          <VisibilityIcon />
-                        ) : (
-                          <VisibilityOffIcon />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                //onClick={}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs></Grid>
-                <Grid item></Grid>
-              </Grid>
-            </Box>
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs></Grid>
+              <Grid item></Grid>
+            </Grid>
           </Box>
-        </Grid>
+        </Box>
       </Grid>
-    </ThemeProvider>
+    </Grid>
   );
 }
